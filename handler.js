02c0b8ca2c0e15979de81079f1cdef8a53d6490c
Peaky Yuter <ajy34@cornell.edu>
2021-01-15 02:03:01 -0800

    finally got my first deploy working!

--- handler.js
+++ handler.js
@@ -1,15 +1,10 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.hellotest2 = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
+      "hellotest2 from severless"
     ),
   };
 
